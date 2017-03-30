c034866a2e1e94505799669d23ce85173d0071c7
Fomoria <icks13@hotmail.com>
2017-03-30 17:27:56 +0200

    changed color to colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324181403) do
+ActiveRecord::Schema.define(version: 20170330145954) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170324181403) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
     t.string   "price"
   end
 
