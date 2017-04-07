e6b8f4e50862b79febe306c2b8d52bc985c6ea61
Fomoria <icks13@hotmail.com>
2017-04-07 23:30:10 +0200

    5.1 added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :orders, only: [:index, :show, :create, :destroy]
-  get 'static_pages/about'
 
-  get 'static_pages/contact'
+	resources :products
+	resources :orders, only: [:index, :show, :create, :destroy]
+  
+	root 'static_pages#index'
 
-  get 'static_pages/index'
+	get 'static_pages/index'
 
-  root 'static_pages#landing_page'
+	get 'static_pages/about'
 
-  post 'static_pages/thank_you'
+	get 'static_pages/contact'
+
+	get 'static_pages/landing_page'
+
+	post 'static_pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
