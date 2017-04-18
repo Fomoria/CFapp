b072a98fd40beb6a1b9c7a8b044803fe1348a361
Fomoria <icks13@hotmail.com>
2017-04-18 05:30:49 +0200

    6.3 added test for user login

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,46 @@
+
+require 'rails_helper'
+
+describe UsersController, :type => :controller do 
+	
+	before do
+		@user = User.create!(email: "rspecuser@test.com", password: "123456")
+		@user2 = User.create!(email: "rspecuser2@test.com", password: "1234567")
+	end
+
+	describe 'GET #show' do
+
+		context "User is logged in" do 
+			before do
+				sign_in @user 
+				get :show, params: { id: @user.id }
+			end 
+
+			it "loads correct user details" do
+				expect(response).to be_success
+				expect(response). to have_http_status(200)
+				expect(assigns(:user)).to eq @user
+			end
+		end
+
+		context "No user is logged in" do 
+			before do
+				get :show, params: { id: @user.id }
+			end
+
+			it "redirects to login" do 
+				expect(response).to redirect_to(new_user_session_path)
+			end
+		end 
+
+		context "User not authorized" do
+			before do 
+				get :show, params: { id: @user2.id }
+			end
+
+			it "redirects to login" do
+				expect(response).to redirect_to(new_user_session_path)
+			end
+		end	
+	end
+end
