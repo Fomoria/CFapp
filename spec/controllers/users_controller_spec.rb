79f2e121022fe1f45a3d8e33e00fa263cc6c8644
Fomoria <icks13@hotmail.com>
2017-04-20 18:34:08 +0200

    6.2 fixed user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,17 +6,19 @@ describe UsersController, :type => :controller do
 	before do
 		@user = FactoryGirl.create(:user)
 		@user2 = User.create!(email: "rspecuser2@test.com", password: "1234567")
-		sign_in @user
 	end
+	
 	describe "GET #show" do
 		context 'User is logged in' do
+			before do
+				sign_in @user
 			it 'loads correct user details' do
 				get :show, params: {id: @user.id}
 				expect(response).to have_http_status(200)
 				expect(assigns(:user)).to eq @user
         	end
       	end
-	end
+	
 		context 'No user is logged in' do
 			it 'redirects to login' do
 				get :show, params: {id: @user.id}
@@ -25,9 +27,13 @@ describe UsersController, :type => :controller do
         	end
       	end
       	context 'User not authorized' do
+      		before do
+      			sign_in @user
+      		end
       		it 'redirects to root path' do
       	  		get :show, params: {id: @user2.id}  
 				expect(response).to redirect_to(root_path)
         	end
       	end
+	end
 end
