85efe1958afe1e6232d3a30c39118600a7203498
Fomoria <icks13@hotmail.com>
2017-04-21 23:23:34 +0200

    6.5 payments controller updated

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,13 +9,17 @@ class PaymentsController < ApplicationController
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        :amount => (@property.price*100).to_i, # amount in cents, again
+        :amount => (@product.price*100).to_i, # amount in cents, again
         :currency => "usd",
         :source => token,
         :description => params[:stripeEmail]
       )
       if charge.paid
-        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+        Order.create(
+          product_id: @product.id, 
+          user_id: @user.id,
+          total: @product.price_in_cents
+          )
       end
     rescue Stripe::CardError => e
       # The card has been declined
