813b573407525645999c541de2ad60fd696f1216
Fomoria <icks13@hotmail.com>
2017-04-22 00:31:42 +0200

    6.6 added post payments create

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,6 +7,7 @@ class PaymentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @user = current_user
     # Create the charge on Stripe's servers - this will charge the user's card
+    
     begin
       charge = Stripe::Charge.create(
         :amount => (@product.price*100).to_i, # amount in cents, again
@@ -14,6 +15,7 @@ class PaymentsController < ApplicationController
         :source => token,
         :description => params[:stripeEmail]
       )
+      
       if charge.paid
         Order.create(
           product_id: @product.id, 
@@ -21,12 +23,14 @@ class PaymentsController < ApplicationController
           total: @product.price_in_cents
           )
       end
+    
     rescue Stripe::CardError => e
       # The card has been declined
       body = e.json_body
       err = body[:error]
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
+    
     redirect_to product_path(@product), notice: 'Purchase was successfully completed.'
   end
 end
