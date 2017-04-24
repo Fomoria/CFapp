55b964f2a32ddddaa11384daf7fb2f99667ffa59
Fomoria <icks13@hotmail.com>
2017-04-25 00:52:39 +0200

    6.12 implemented redis/rails in product and product/show

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,4 +24,14 @@ class Product < ApplicationRecord
       comments.average(:rating).to_f
   end
 
+  # models/product_view.rb
+
+  def views
+    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+  end
+
+  def viewed!
+    $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
+  end
+
 end
