bf2c8be251adc840396b68cfa46a7457d027dddc
Fomoria <icks13@hotmail.com>
2017-05-02 00:45:45 +0200

    sign up works again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
-  devise_for :users, :controllers => { :registrations => "user_registrations"}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
+  
+  get 'user_mailer/contact_form'
+
+
   resources :products do
     resources :comments
   end
